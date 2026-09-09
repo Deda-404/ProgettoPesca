368d3985de6868e8df89024de0827aeb1e3f339f
Alessio de Dato <107488474+Deda-404@users.noreply.github.com>
2026-09-09 15:20:01 +0200

    style: load catch geolocation styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './forecast.css'
+import './catchGeo.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
